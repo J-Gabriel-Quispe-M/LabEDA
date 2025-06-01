841669ee769ee870a143a685917bc01ed081c07f
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-05-31 21:42:21 -0500

    Implementación de un nodo básico para listas enlazadas circulares, con un campo de datos (data) y un enlace al siguiente nodo (next).

---
+++ Laboratorio 4/src/ejercicio7/NodoCircular.java
@@ -0,0 +1,11 @@
+package ejercicio7;
+
+public class NodoCircular {
+    int data;
+    NodoCircular next;
+
+    public NodoCircular(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
