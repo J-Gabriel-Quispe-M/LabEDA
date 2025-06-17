7497a47b95f77794d8346fb5d06d597163ba7078
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-06-16 22:39:53 -0500

    Implementación de la clase Nodo genérica para representar un nodo del árbol binario de búsqueda.

---
+++ Laboratorio 6/src/ejercicio2/Nodo.java
@@ -0,0 +1,12 @@
+package ejercicio2;
+
+public class Nodo<T> {
+    T dato;
+    Nodo<T> izquierdo, derecho;
+
+    public Nodo(T dato) {
+        this.dato = dato;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+}
