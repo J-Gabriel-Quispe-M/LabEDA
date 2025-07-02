291cb652bc662d6940f8435a8784b97571be1316
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-07-01 22:25:35 -0500

    Clase NodoAVL para representar nodos genéricos del árbol AVL

---
+++ Laboratorio 7/src/ejercicio3/NodoAVL.java
@@ -0,0 +1,13 @@
+package ejercicio3;
+
+public class NodoAVL<T extends Comparable<T>> {
+    public T dato;
+    public NodoAVL<T> izquierdo;
+    public NodoAVL<T> derecho;
+    public int altura;
+
+    public NodoAVL(T dato) {
+        this.dato = dato;
+        this.altura = 1;
+    }
+}
