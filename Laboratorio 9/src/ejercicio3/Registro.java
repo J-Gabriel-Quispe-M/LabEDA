ef53c67712b1d3e7b67c0662083af1b8cf54b22d
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-07-15 13:10:21 -0500

    Crear clase Registro con clave y valor para uso en hash cerrado

---
+++ Laboratorio 9/src/ejercicio3/Registro.java
@@ -0,0 +1,31 @@
+package ejercicio3;
+
+public class Registro<E> implements Comparable<Registro<E>> {
+    private int clave;
+    private E valor;
+
+    public Registro(int clave, E valor) {
+        this.clave = clave;
+        this.valor = valor;
+    }
+
+    public int obtenerClave() {
+        return clave;
+    }
+
+    public E obtenerValor() {
+        return valor;
+    }
+
+    public void establecerValor(E valor) {
+        this.valor = valor;
+    }
+
+    public int compareTo(Registro<E> otro) {
+        return Integer.compare(this.clave, otro.clave);
+    }
+
+    public String toString() {
+        return clave + ": " + valor;
+    }
+}
